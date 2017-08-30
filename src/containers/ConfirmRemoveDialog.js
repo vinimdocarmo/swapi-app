d0b8bc209437e463a385a82f97bf2cdd678f51e6
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2017-08-30 02:50:49 -0300

    Add a dialog to confirm a planet removal.

---
+++ src/containers/ConfirmRemoveDialog.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Dialog, {DialogTitle, DialogActions} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import PropTypes from 'prop-types';
+
+class ConfirmRemoveDialog extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Dialog open={this.props.open} onRequestClose={() => this.props.onRequestClose(false)}>
+                <DialogTitle>
+                    {"Confirm item removal?"}
+                </DialogTitle>
+                <DialogActions>
+                    <Button onClick={() => this.props.onRequestClose(false)} color="primary">
+                        Disagree
+                    </Button>
+                    <Button onClick={() => this.props.onRequestClose(true)} color="primary">
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
+
+ConfirmRemoveDialog.propTypes = {
+    onRequestClose: PropTypes.func,
+    open: PropTypes.bool
+};
+
+export default ConfirmRemoveDialog;
--- src/containers/PlanetsList.js
+++ src/containers/PlanetsList.js
@@ -9,15 +9,18 @@ import fetchPlanetsNextPage from '../actions/fetchPlanetsNextPage';
 import {connect} from 'react-redux';
 import NextPageButton from './NextPageButton';
 import PlanetDialog from './PlanetDialog';
+import ConfirmRemoveDialog from './ConfirmRemoveDialog';
 import {MODES as PLANET_DIALOG_MODES} from './PlanetDialog';
 
 class PlanetsList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {planets: props.planets, selectedPlanet: null, planetDialogOpen: false};
+        this.state = {planets: props.planets, selectedPlanet: null, planetDialogOpen: false, removeDialogOpen: false};
         this.canFetchNextPage = this.canFetchNextPage.bind(this);
         this.handleOpenEdition = this.handleOpenEdition.bind(this);
-        this.handleRequestClose = this.handleRequestClose.bind(this);
+        this.handleOpenRemoval = this.handleOpenRemoval.bind(this);
+        this.handleEditionRequestClone = this.handleEditionRequestClone.bind(this);
+        this.handleRemovalRequestClone = this.handleRemovalRequestClone.bind(this);
     }
 
     componentWillReceiveProps({planets}) {
@@ -32,8 +35,19 @@ class PlanetsList extends React.Component {
         this.setState({selectedPlanet, planetDialogOpen: true});
     }
 
-    handleRequestClose() {
+    handleOpenRemoval(selectedPlanet) {
+        this.setState({selectedPlanet, removeDialogOpen: true});
+    }
+
+    handleEditionRequestClone() {
         this.setState({planetDialogOpen: false});
+    }
+
+    handleRemovalRequestClone(confirm) {
+        if (confirm) {
+            this.props.removePlanet(this.state.selectedPlanet);
+        }
+        this.setState({removeDialogOpen: false});
     }
 
     render() {
@@ -48,7 +62,7 @@ class PlanetsList extends React.Component {
                                 <ListItemText primary={planet.name}/>
                                 <ListItemSecondaryAction>
                                     <IconButton aria-label="Delete"
-                                                onClick={() => this.props.removePlanet(planet)}>
+                                                onClick={() => this.handleOpenRemoval(planet)}>
                                         <DeleteIcon/>
                                     </IconButton>
                                 </ListItemSecondaryAction>
@@ -58,8 +72,9 @@ class PlanetsList extends React.Component {
                 </List>
                 <NextPageButton disabled={!this.canFetchNextPage()}
                                 onNextPage={this.props.fetchPlanetsNextPage}/>
-                <PlanetDialog open={this.state.planetDialogOpen} onRequestClose={this.handleRequestClose}
+                <PlanetDialog open={this.state.planetDialogOpen} onRequestClose={this.handleEditionRequestClone}
                               mode={PLANET_DIALOG_MODES.EDIT} planet={this.state.selectedPlanet}/>
+                <ConfirmRemoveDialog open={this.state.removeDialogOpen} onRequestClose={this.handleRemovalRequestClone}/>
             </div>
         );
     }
